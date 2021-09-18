87aa8e11686775031b5b797f12e3ea46fed86fb1
Cody Stine <cydoentis@gmail.com>
2021-09-18 16:47:35 -0400

    Added active selector/functionality to navigation

--- src/components/Nav/NavItem.jsx
+++ src/components/Nav/NavItem.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 import styles from "./NavItem.module.scss";
 
-const NavItem = ({ icon, text }) => {
+const NavItem = ({ className, icon, text, onClick }) => {
 	return (
-		<li className={styles["nav-item"]}>
+		<li className={`${className} ${styles["nav-item"]}`} onClick={onClick}>
 			<i className={`${icon} ${styles["nav-item--icon"]}`} />
 			<p className={styles["nav-item--text"]}>{text}</p>
 		</li>
--- src/components/Nav/NavMenu.jsx
+++ src/components/Nav/NavMenu.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import NavItem from "./NavItem";
 
 import styles from "./NavMenu.module.scss";
 
 const NavMenu = () => {
-	const aboutIcon = "bx bxs-user";
-	const resumeIcon = "bx bxs-file";
-	const workIcon = "bx bxs-folder";
-	const contactIcon = "bx bxs-message-dots";
+	const [activeState, setActiveState] = useState({
+		activeItem: null,
+		items: [
+			{ id: 1, text: "About", icon: "bx bxs-user" },
+			{ id: 2, text: "Resume", icon: "bx bxs-file" },
+			{ id: 3, text: "Work", icon: "bx bxs-folder" },
+			{ id: 4, text: "Contact", icon: "bx bxs-message-dots" },
+		],
+	});
+
+	const toggleActiveState = (index) => {
+		setActiveState({ ...activeState, activeItem: activeState.items[index] });
+	};
+
+	const toggleActiveClass = (index) => {
+		if (activeState.items[index] === activeState.activeItem) return `${styles.active}`;
+		else return "";
+	};
 
 	return (
 		<ul className={styles["nav-menu"]}>
-			<NavItem text={"About"} icon={aboutIcon} />
-			<NavItem text={"Resume"} icon={resumeIcon} />
-			<NavItem text={"Work"} icon={workIcon} />
-			<NavItem text={"Contact"} icon={contactIcon} />
+			{activeState.items.map((item, index) => (
+				<NavItem
+					key={index}
+					className={toggleActiveClass(index)}
+					text={item.text}
+					icon={item.icon}
+					onClick={() => {
+						toggleActiveState(index);
+					}}
+				/>
+			))}
 		</ul>
 	);
 };
