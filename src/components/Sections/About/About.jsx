eda2e13ca0d6de73819f18f842190221cd0ce5ab
Cody Stine <cydoentis@gmail.com>
2021-09-30 12:53:34 -0400

    Added updated resume

--- src/components/Sections/About/About.jsx
+++ src/components/Sections/About/About.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { Link as ReactLink } from "react-scroll";
-import Link from "../../UI/Links/Link";
+import React from 'react';
+import { Link as ReactLink } from 'react-scroll';
+import Link from '../../UI/Links/Link';
 
-import resume from "../../../assets/Cody-Stine-Resume.pdf";
+import resume from '../../../assets/Resume.pdf';
 
-import styles from "./About.module.scss";
+import styles from './About.module.scss';
 
 const About = () => {
 	return (
@@ -14,22 +14,22 @@ const About = () => {
 			</h1>
 			<p>Web Developer</p>
 			<div className={styles.socials}>
-				<Link className={"bxl-github"} link={"https://github.com/CydoEntis"} />{" "}
+				<Link className={'bxl-github'} link={'https://github.com/CydoEntis'} />{' '}
 				<Link
-					className={"bxl-linkedin"}
-					link={"https://www.linkedin.com/in/cody-stine-06857814a/"}
+					className={'bxl-linkedin'}
+					link={'https://www.linkedin.com/in/cody-stine-06857814a/'}
 				/>
 			</div>
-			<div className={styles["about--controls"]}>
+			<div className={styles['about--controls']}>
 				<a
 					href={resume}
 					download
-					className={`${styles["about-btn"]} ${styles["about-btn--border"]}`}>
+					className={`${styles['about-btn']} ${styles['about-btn--border']}`}>
 					Download Cv
 				</a>
 				<ReactLink
-					className={styles["about-btn"]}
-					to={"contact"}
+					className={styles['about-btn']}
+					to={'contact'}
 					spy={true}
 					smooth={true}
 					offset={-64}
